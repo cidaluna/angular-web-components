e1269c407305977a5ef04362eabeccb7eeeabba4
Cida Luna <cidalunna@hotmail.com>
2024-11-09 17:44:13 -0300

    Add angular material and configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      CommonModule,
+      ReactiveFormsModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      FormsModule,
+    ), provideAnimationsAsync()
+  ]
 };
