e1bd3060c855eb66a21707523667af1ae4793b79
Cida Luna <cidalunna@hotmail.com>
2024-11-09 11:09:56 -0300

    Add address component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AddressFormComponent } from "./components/address-form/address-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AddressFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
